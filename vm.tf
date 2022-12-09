8d8b8530f9364daa0aedf48b75c362de8c7cd030
gesazumoo <gesazumoo@huinnoaim.com>
2022-12-09 17:07:10 +0900

    포기;

--- vm.tf
+++ vm.tf
@@ -1,8 +1,8 @@
-resource "proxmox_vm_qemu" "cloudinit_test" {
-  name        = "tftest1"
+resource "proxmox_vm_qemu" "cloudinit_test2" {
+  name        = "tftest2"
   desc        = "tf description"
   target_node = "pve"
-  os_type     = "cloud-init"
+  os_type     = "ubuntu"
   clone       = "baseubuntu"
 
   disk {
@@ -11,14 +11,22 @@ resource "proxmox_vm_qemu" "cloudinit_test" {
     storage = "local-lvm"
   }
 
+  full_clone = false
+
   network {
     bridge = "vmbr0"
     model  = "virtio"
   }
 
-  cores   = 3
-  sockets = 1
-  memory  = 2560
+  # define_connection_info = true
+  # os_network_config      = <<EOF
+  # EOF
+  cores                  = 3
+  sockets                = 1
+  memory                 = 2560
 
-  ipconfig0 = "ip=192.168.123.55/16,gw=10.0.2.2"
+# 그냥 올리고 ip는 수동으로...
+# 템플릿에서 gesazumoo 계정 추가된걸로 변경하고
+# docker, docker compose 설치.
+# ansible로 ect/opt아래에 docker-compose.yml 파일 만들기
 }
