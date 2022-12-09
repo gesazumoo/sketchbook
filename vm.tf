53de4a2f49e4cddf01f7f6086ae9bca49e616b8b
gesazumoo <gesazumoo@huinnoaim.com>
2022-12-09 11:42:07 +0900

    일단 만들어진다

--- vm.tf
+++ vm.tf
@@ -1,8 +1,24 @@
-resource "proxmox_vm_qemu" "proxmox_master" {
-  name        = "terraformvm"
+resource "proxmox_vm_qemu" "cloudinit_test" {
+  name        = "tftest1"
+  desc        = "tf description"
   target_node = "pve"
-  vmid = 11
-  desc = "this is desc"
+  os_type     = "cloud-init"
   clone       = "baseubuntu"
 
+  disk {
+    type    = "scsi"
+    size    = "40G"
+    storage = "local-lvm"
+  }
+
+  network {
+    bridge = "vmbr0"
+    model  = "virtio"
+  }
+
+  cores   = 3
+  sockets = 1
+  memory  = 2560
+
+  ipconfig0 = "ip=192.168.123.55/16,gw=10.0.2.2"
 }
